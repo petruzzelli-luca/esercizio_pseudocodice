9e79a715c3b36f8c4bae04cde966f700e237ea30
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-09-17 08:52:27 +0000

    primo commit

---
+++ codice.js
@@ -0,0 +1,16 @@
+// fattoriale di un n
+function factorial(n) {
+    
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+// Example usage:
+const number = 5;
+const result = factorial(number);
+console.log(`The factorial of ${number} is ${result}`);
